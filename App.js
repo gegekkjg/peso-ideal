b11ded2ed2037449e7e22559ef4117357aff2c13
jmmjml <116905479+jmmjml@users.noreply.github.com>
2023-09-13 12:59:35 +0000

    asjhfg

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, Text, View,Switch, TextInput } from 'react-native';
+import { Pressable, Text, View,Switch, TextInput, Image } from 'react-native';
 import styles from './globalStyles';
 
 // http://conversorfacil.com.br/calcular/peso-ideal
+import imagem1 from './assets/men.jpg'
+import imagem2 from './assets/woman.jpg'
 
 export default function App() {
   const [altura, setAltura] = useState('');
@@ -19,7 +21,6 @@ export default function App() {
       setPeso(( h - 100 ) - [ ( h - 150 ) / 2 ])
     }
   };
-  
   return (
     <View style={styles.container}>
       <View style={styles.content}>
@@ -33,7 +34,8 @@ export default function App() {
           />
         </View>
         <Text>Sexo</Text>
-        <View>
+        <View style={{display: "flex", flexDirection: "row",  alignItems: "center", justifyContent: "center"}}>
+          <Text>Homem</Text>
           <Switch
                 trackColor={{ false: '#b1deef', true: '#b1deef' }}
                 thumbColor={sexo ? '#298DC7' : '#298DC7'}
@@ -41,18 +43,24 @@ export default function App() {
                 onValueChange={setSexo}
                 value={sexo}
           />
+          <Text>Mulher</Text>
         </View>
         <Pressable
-          style={{}}
+          style={{width: '25%', backgroundColor: "#000"}}
           onPress={() => calcular(altura,sexo,peso)}
         >
-          <Text>
+          <Text style={{color: "#fff"}}>
             calcular
           </Text>
         </Pressable>
 
         <View><Text>{`${peso}`}</Text></View>
+        <Image
+        source={imagem1}
+        style={{width:"30%", height:"50%"}}
+      />
         <StatusBar style="auto" />
+        
       </View>
     </View>
   );
